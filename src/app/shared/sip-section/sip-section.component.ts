c832d1043f40ed821ae628ee3136c43bf96d3fa0
Avirup <avirupduttagupta@gmail.com>
2023-10-03 13:44:56 +0530

    Update sip-section.component.ts

--- src/app/shared/sip-section/sip-section.component.ts
+++ src/app/shared/sip-section/sip-section.component.ts
@@ -13,9 +13,9 @@ export class SipSectionComponent {
   @Output() valueChangeEvent = new EventEmitter<any>();
   constructor(private _formBuilder: FormBuilder) {
     this.sipAmountForm = this._formBuilder.group({
-      'FD/RD': ['0'],
-      'Debt Mutual Fund': ['0'],
-      'Equity Mutual Fund': ['0'],
+      'FD/RD': [0],
+      'Debt Mutual Fund': [0],
+      'Equity Mutual Fund': [0],
     });
   }
   ngOnInit(): void {
